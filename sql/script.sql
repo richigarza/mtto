8761bc3da91510febba810896f9e94a0fccafbee
richigarza <richi.garzar@gmail.com>
2014-11-10 01:57:07 -0600

    cambios sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,3 +1,3 @@
 ALTER TABLE  `UNIDAD_EQUIPO` ADD  `proximo_mantenimiento` TIMESTAMP NOT NULL COMMENT  'El mtto preventivo' AFTER  `ultimo_mantenimiento` ;
 
-
+ALTER TABLE  `EQUIPO` ADD  `tiempo_mantenimiento` INT( 2 ) NOT NULL COMMENT  'Cada cuantos dias se da el mtto preventivo' AFTER  `descripcion` ;
